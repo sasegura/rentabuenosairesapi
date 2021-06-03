120f878facfff63ad9d42eb8876ff319596a84a9
sasegura <sasegura.fernandez87@gmail.com>
2021-06-04 01:15:00 +0200

    asd

--- src/models/usuario.model.ts
+++ src/models/usuario.model.ts
@@ -6,7 +6,7 @@ import {Entity, model, property} from '@loopback/repository';
 export class Usuario extends Entity {
   @property({
     type: 'number',
-    required: true,
+    required: false,
     scale: 0,
     id: 1,
     postgresql: {columnName: 'idusuario', dataType: 'integer', dataLength: null, dataPrecision: null, dataScale: 0, nullable: 'NO'},
