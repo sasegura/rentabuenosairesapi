7c3b8de6485bc8c4eb8aa63b776bc684d0418437
sasegura <sasegura.fernandez87@gmail.com>
2021-09-21 20:03:50 +0200

    pass

--- src/services/notification.services.ts
+++ src/services/notification.services.ts
@@ -1,20 +1,18 @@
-
 import {SentMessageInfo} from 'nodemailer';
 import Mail = require('nodemailer/lib/mailer');
 
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 export class MailerService {
-
   async sendMail(mailOptions: Mail.Options): Promise<SentMessageInfo> {
     const transporter = nodemailer.createTransport({
-      host: "smtp.servidor-correo.net",
+      host: 'smtp.servidor-correo.net',
       port: 587,
       secure: false, // use TLS
       auth: {
-        user: "administrador@e-homeselect.com",
-        pass: "Jonaberm1983"
-      }
+        user: 'administrador@e-homeselect.com',
+        pass: 'Jonaberm83',
+      },
     });
     return await transporter.sendMail(mailOptions);
   }
