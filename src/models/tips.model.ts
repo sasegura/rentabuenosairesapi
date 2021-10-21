686a81df9c3885a03b1973e9e497f4c80cb205a4
sasegura <sasegura.fernandez87@gmail.com>
2021-10-21 22:43:41 +0200

    idpiso en los tips

--- src/models/tips.model.ts
+++ src/models/tips.model.ts
@@ -1,20 +1,36 @@
 import {Entity, model, property} from '@loopback/repository';
 
-@model({settings: {idInjection: false, postgresql: {schema: 'public', table: 'tips'}}})
+@model({
+  settings: {idInjection: false, postgresql: {schema: 'public', table: 'tips'}},
+})
 export class Tips extends Entity {
   @property({
     type: 'number',
     required: true,
     scale: 0,
     id: 1,
-    postgresql: {columnName: 'idtips', dataType: 'integer', dataLength: null, dataPrecision: null, dataScale: 0, nullable: 'NO'},
+    postgresql: {
+      columnName: 'idtips',
+      dataType: 'integer',
+      dataLength: null,
+      dataPrecision: null,
+      dataScale: 0,
+      nullable: 'NO',
+    },
   })
   idtips: number;
 
   @property({
     type: 'string',
     required: true,
-    postgresql: {columnName: 'nombre', dataType: 'text', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'NO'},
+    postgresql: {
+      columnName: 'nombre',
+      dataType: 'text',
+      dataLength: null,
+      dataPrecision: null,
+      dataScale: null,
+      nullable: 'NO',
+    },
   })
   nombre: string;
 
@@ -22,15 +38,43 @@ export class Tips extends Entity {
     type: 'number',
     required: true,
     scale: 0,
-    postgresql: {columnName: 'idcoordenada', dataType: 'integer', dataLength: null, dataPrecision: null, dataScale: 0, nullable: 'NO'},
+    postgresql: {
+      columnName: 'idcoordenada',
+      dataType: 'integer',
+      dataLength: null,
+      dataPrecision: null,
+      dataScale: 0,
+      nullable: 'NO',
+    },
   })
   idcoordenada: number;
 
   @property({
     type: 'string',
-    postgresql: {columnName: 'descripcion', dataType: 'text', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'YES'},
+    postgresql: {
+      columnName: 'descripcion',
+      dataType: 'text',
+      dataLength: null,
+      dataPrecision: null,
+      dataScale: null,
+      nullable: 'YES',
+    },
   })
   descripcion?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+    postgresql: {
+      columnName: 'idpiso',
+      dataType: 'text',
+      dataLength: null,
+      dataPrecision: null,
+      dataScale: null,
+      nullable: 'NO',
+    },
+  })
+  idpiso: string;
 
   // Define well-known properties here
 
